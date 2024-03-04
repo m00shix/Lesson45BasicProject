2731a25eca882055cdd578138e1815d457dcd441
m00shix <str41k@mail.ru>
2024-03-04 18:17:04 +0300

    updated logic & main

--- task1/logic.cpp
+++ task1/logic.cpp
@@ -1,28 +1,7 @@
 #include <iostream>
+#include <string>
 
-int sum_negative_elem(int* array, int size, int min, int max){
-
-	int sum = 0;
-
-	for (int i = 0; i < size; i++)
-	{
-		array[i] = rand() % (max - min + 1) + min;
-	}
-}
-
-int max_elem(int* array, int size) {
-
-	int max = 0;
-
-	for (int i = 0; i < size; i++)
-	{
-		if (max > array[i])
-		{
-			max = i;
-		}
-	}
-	return max;
-}
+using namespace std;
 
 void rand_init(int* array, int size, int min, int max) {
 	for (int i = 0; i < size; i++)
@@ -31,6 +10,80 @@ void rand_init(int* array, int size, int min, int max) {
 	}
 }
 
-int max_min_elem(int* array, int size) {
+string convert_array_string(int* array, int size) {
+	string msg = "";
 
+	for (int i = 0; i < size; i++)
+	{
+		msg += to_string(array[i]) + " ";
+	}
+	return msg;
+}
+
+int negative_element_sum(int* array, int size) {
+
+	int sum = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] < 0) {
+			sum += array[i];
+		}
+	}
+	return sum;
+}
+
+int max_element_index(int* array, int size) {
+
+	int max = 0;
+
+	for (int i = 1; i < size; i++)
+	{
+		if (array[max] < array[i])
+		{
+			max = i;
+		}
+	}
+	return max;
+}
+
+int min_element_index(int* array, int size) {
+
+	int min = 0;
+
+	for (int i = 1; i < size; i++)
+	{
+		if (array[min] > array[i])
+		{
+			min = i;
+		}
+	}
+	return min;
+}
+
+int product_betweeen_min_max(int* array, int size) {
+
+	int min = min_element_index(array, size);
+	int max = max_element_index(array, size);
+
+	if (abs(min - max) < 2)
+	{
+		return 0;
+	}
+
+	if (min > max)
+	{
+		int t = min;
+		min = max;
+		max = t;
+	}
+
+	int product = 1;
+
+	for (int i = min + 1; i < max; i++)
+	{
+		product *= array[i];
+	}
+
+	return product;
 }
--- task1/main.cpp
+++ task1/main.cpp
@@ -2,18 +2,31 @@
 
 using namespace std;
 
-int sum_negative_elem(int* array, int size, int min, int max);
-int max_elem(int* array, int size);
 void rand_init(int* array, int size, int min, int max);
+string convert_array_string(int* array, int size);
+int negative_element_sum(int* array, int size);
+int max_element_index(int* array, int size);
+int min_element_index(int* array, int size);
+int product_betweeen_min_max(int* array, int size);
 
 int main() {
 	int size = 0;
-	do {
-		cout << "Input number: ";
+
+	do
+	{
+		cout << "Input array size : ";
 		cin >> size;
 	} while (size < 1);
 
+	int* array = new int[size];
 
+	rand_init(array, size, -10, 10);
+	cout << convert_array_string(array, size);
+	cout << "\nSum of negative elements is " << negative_element_sum(array, size);
+	cout << "\nProduct of elements between min and max is "
+		<< product_betweeen_min_max(array, size);
+
+	delete[] array;
 
 	return 0;
 }
